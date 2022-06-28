b3ddc2aa869ec1576de243254b84ab7e5760ffb6
nikiforovp <108361968+nikiforovp@users.noreply.github.com>
2022-06-28 21:48:25 +0300

    Lesson1

--- Main.java
+++ Main.java
@@ -49,11 +49,55 @@ public class Main {
         */
 
         //Begin8. Даны два числа a и b. Найти их среднее арифметическое: (a + b)/2.
+        /*
         int a = 3;
         int b = 4;
         int d = 2;
         double c = (a + b) / (double)d;
         System.out.println("Среднее арифметическое " + c);
-
+         */
+        //Begin9. Даны два неотрицательных числа a и b. Найти их среднее геометрическое,
+        // то есть квадратный корень из их произведения: √a·b.
+        /*double a = 3.45;
+        double b = 7.2;
+        double g_avg = Math.sqrt(a * b);
+        System.out.println(g_avg);*/
+//        Begin10. Даны два ненулевых числа. Найти сумму, разность, произведение и
+//        частное их квадратов.
+        /*int a = 25;
+        int b = 45;
+        double s = (a * a) + (b * b);
+        System.out.println("Сумма: " + (int)s);
+        s = (b * b) - (a + a);
+        System.out.println("Разность: " + s);
+        s = (a * a) * (a * a);
+        System.out.println("Произведение: " + s);
+        s = (a * a) / (double) (b * b);
+        System.out.println("Частное: " + s);
+        */
+//        Begin11. Даны два ненулевых числа. Найти сумму, разность, произведение и
+//        частное их модулей.
+        /*int a = -5;
+        int b = 24;
+        a = Math.abs(a);
+        b = Math.abs(b);
+        double s = a + b;
+        System.out.println("Сумма модулей " + (int)s);
+        s = a - b;
+        System.out.println("Разность модулей " + (int)s);
+        s = a * b;
+        System.out.println("Произведение модулей " + (int)s);
+        s = (double)a / b;
+        System.out.println("Частное модулей " + s);
+        */
+        //Begin22. Поменять местами содержимое переменных A и B и вывести новые
+        //значения A и B.
+        /*int a = 3;
+        int b = 10;
+        int c = a;
+        a = b;
+        b = c;
+        System.out.println("a = " + a + " b = " + b);*/
+        
     }
 }
